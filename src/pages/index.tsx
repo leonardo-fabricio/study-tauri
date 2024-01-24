554595ec19c6c682e2439b700848fd4b562108f1
leonardo-fabricio <leonnardofabbricio@gmail.com>
2024-01-23 21:58:31 -0300

    commitando codigo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,27 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import styled from "styled-components";
+import { colorTheme } from "@/styles/colors";
+import Header from "./componentes/Header";
+import Main from "./componentes/Main";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
-      <h1>Meu app</h1>
-    </>
+    <PageContainer>
+      <Header/>
+      <Main/>
+    </PageContainer>
   );
 }
+
+const PageContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: ${colorTheme.background};
+  color: black;
+  display: flex;
+gap: 12px;
+`
